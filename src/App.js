ad0a7c5be2cd7e7c7adcf1f3559c369dffda6a79
Baran Yılmaz <92272002+kabarmaz@users.noreply.github.com>
2022-10-04 16:43:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Index from "./index.html"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <html src=Index>
+    <html>
+      
 }
 
 export default App;
